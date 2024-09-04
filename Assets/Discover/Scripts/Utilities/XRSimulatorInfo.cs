0357c9af5b2abe51a53a685f4441e5ba6a36e15b
Alexandre Thivierge <athivierge@meta.com>
2024-09-03 21:13:50 -0700

    Update SDKs to v68

--- Assets/Discover/Scripts/Utilities/XRSimulatorInfo.cs
+++ Assets/Discover/Scripts/Utilities/XRSimulatorInfo.cs
@@ -8,13 +8,20 @@ namespace Discover.Utilities
 {
     public static class XRSimulatorInfo
     {
-        private const string SYNTH_ENV_SERVER_PORT = "33792"; // from MetaXRSimulatorEnabler
+        // As of XR Simulator v68, there is function to know if the Synthetic environment is running.
+        // private const string SYNTH_ENV_SERVER_PORT = "33792"; // from MetaXRSimulatorEnabler
 
         private static string RuntimeJson => Environment.GetEnvironmentVariable("XR_RUNTIME_JSON");
 
         public static bool IsSimulatorActivated() => RuntimeJson?.Contains("meta_openxr_simulator") is true;
-
-        public static bool IsSynthEnvActivated() => MetaXRSimulatorEnabler.IsProcessRunning(SYNTH_ENV_SERVER_PORT);
+        
+        // TODO fix in future version:
+        // As of XR Simulator v68, there is function to know if the Synthetic environment is running.
+        public static bool IsSynthEnvActivated() =>
+            Meta.XR.Simulator.Editor.Enabler.
+                Activated;
+        // This was the call from v63
+        // MetaXRSimulatorEnabler.IsProcessRunning(SYNTH_ENV_SERVER_PORT);
     }
 }
 
